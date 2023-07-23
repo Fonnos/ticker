182504e67e6eae795e76a3951e37957c2332d8ff
fonnos <eric.panhans@gmail.com>
2023-07-23 17:42:49 +0200

    v0.2

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-// Add your news here
-const news = [
-    "Hier könnten aktuelle announcements stehen!",
-    "Natürlich in aller Breaking News Manier",
-    "danke das ihr mich anhört"
-  ];
+// script.js
+const ticker = document.getElementById("ticker");
 
-  //this is a test
-  
-  const ticker = document.getElementById("ticker");
-  
-  function displayNews() {
-    ticker.innerHTML = ""; // Clear previous news
-    for (let i = 0; i < news.length; i++) {
-      const listItem = document.createElement("li");
-      listItem.textContent = news[i];
-      listItem.classList.add("news-item");
-      ticker.appendChild(listItem);
-    }
+function calculateAnimationDuration() {
+  const tickerItems = ticker.querySelectorAll(".news-item");
+  let totalWidth = 0;
+  tickerItems.forEach(item => {
+    totalWidth += item.offsetWidth;
+  });
+  const containerWidth = ticker.offsetWidth;
+  const animationDuration = totalWidth / 100; // Adjust the divisor to control the animation speed
+  return animationDuration;
+}
+
+function displayNews() {
+  ticker.innerHTML = ""; // Clear previous news
+  for (let i = 0; i < news.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = news[i];
+    listItem.classList.add("news-item");
+    ticker.appendChild(listItem);
   }
-  
-  // Display the news every 3 seconds (adjust the interval as needed)
-  setInterval(displayNews, 3000);
-  
-  // Initial display
-  displayNews();
-  
-  
+
+  const animationDuration = calculateAnimationDuration();
+  ticker.style.animationDuration = `${animationDuration}s`;
+}
+
+// Initial news array
+let news = [
+  "Breaking News: Event X is happening now!",
+  "New Product Launch: Product Y is now available!",
+  "Weather Update: Expect heavy rainfall tomorrow.",
+  "Sports News: Team A wins the championship!",
+  "Community Update: Join us for the upcoming event."
+];
+
+// Display the initial news
+displayNews();
